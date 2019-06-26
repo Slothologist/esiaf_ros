49c2421a57df713bdb71be4dcc1974685fc4e5bd
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-06-26 15:34:27 +0200

    grabber has now a adjustable frame size, which will determine the number of frames in a AugmentedAudio msg

--- esiaf_ros/src/nodes/grabber.cpp
+++ esiaf_ros/src/nodes/grabber.cpp
@@ -39,7 +39,6 @@ int main(int argc, char **argv) {
     // alsa initialisation
     int i;
     int err;
-    int buffersize = 8000;
     snd_pcm_t *capture_handle;
     snd_pcm_hw_params_t *hw_params;
 
@@ -51,6 +50,7 @@ int main(int argc, char **argv) {
                                                                         pt.get<char>("int_float"));
     esiaf_ros::Endian endian = esiaf_ros::utils::cfg_to_esiaf_endian(pt.get<std::string>("endian"));
     unsigned int channels = pt.get<unsigned int>("channels");
+    int buffersize = pt.get<unsigned int>("frame_size");
 
     _snd_pcm_format format = esiaf_ros::utils::set_up_format_from_bitrate_and_endian(bitrate, endian);
     unsigned int sample_rate_ext = esiaf_ros::utils::set_up_sample_rate_from_esiaf(sample_rate);
